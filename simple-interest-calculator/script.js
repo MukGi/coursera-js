f23f923365312012b11ece0caf39ac6f0ee4d2bd
GM <t1mukgi@gmail.com>
2026-01-31 01:36:18 -0500

    SIC: JS Update

--- simple-interest-calculator/script.js
+++ simple-interest-calculator/script.js
@@ -1,6 +1,20 @@
-function compute()
-{
+const rate = document.getElementById('rate')
+const rate_val = document.getElementById('rate_val')
+
+
+
+
+
+function compute(){
+const principal = parseInt(document.getElementById('principal').value)
+const years =  document.getElementById('years')
+const interest = document.getElementById('interest')
     p = document.getElementById("principal").value;
     
 }
         
+function updateRate() {
+  
+
+    rate_val.innerText = rate.value
+}
