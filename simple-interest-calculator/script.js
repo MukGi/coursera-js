99764fa3762ba8a32c83d4a45b815a1d40f98ba3
GM <t1mukgi@gmail.com>
2026-02-17 18:06:19 -0500

    SIC: Code completion

--- simple-interest-calculator/script.js
+++ simple-interest-calculator/script.js
@@ -1,20 +1,38 @@
-const rate = document.getElementById('rate')
-const rate_val = document.getElementById('rate_val')
+const rate_slider = document.getElementById('rate_slider')
+const rate_value = document.getElementById('rate_value')
+const yearInput =  document.getElementById('years')
+const principalInput = document.getElementById('principal')
+const result = document.getElementById('result')
 
-
-
-
+function convertYear(year){
+ return newYear = new Date().getFullYear() + year
+}
 
 function compute(){
-const principal = parseInt(document.getElementById('principal').value)
-const years =  document.getElementById('years')
-const interest = document.getElementById('interest')
-    p = document.getElementById("principal").value;
+    const principal = parseInt(principalInput.value)
+    const year = parseInt(yearInput.value,10)
+    const rate = parseFloat(rate_slider.value)
     
-}
-        
-function updateRate() {
-  
+    
+    if (principal <= 0 ) {
+        alert("Enter a valid principal value")
+        principal.focus()
+    }
+    else
+    {
+        const interest = (principal * rate * year)/100
 
-    rate_val.innerText = rate.value
+        result.innerHTML = `If you deposit $${principal},\<br>
+        at an interest rate of ${rate}%.\<br>
+        You will receive an amount of $${interest},\<br>
+        in the year ${convertYear(year)}\<br>`
+    } 
 }
+
+
+
+function updateRate() {
+    rate_value.innerText = rate_slider.value
+}
+
+rate_slider.onchange = updateRate
